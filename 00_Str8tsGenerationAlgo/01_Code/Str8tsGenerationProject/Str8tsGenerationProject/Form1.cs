944a8991d62e7ce25c9b593a95d6ba67c992a602
Mimand3r <hbecker89@aol.de>
2022-01-01 14:30:59 +0100

    Basic Board wurde erzeugt

--- 00_Str8tsGenerationAlgo/01_Code/Str8tsGenerationProject/Str8tsGenerationProject/Form1.cs
+++ 00_Str8tsGenerationAlgo/01_Code/Str8tsGenerationProject/Str8tsGenerationProject/Form1.cs
@@ -15,6 +15,41 @@ namespace Str8tsGenerationProject
         public Form1()
         {
             InitializeComponent();
+
+         
+        }
+
+        private String selectedFile = null;
+
+        private void button_import_Click(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "JSON files (*.json)|*.json;";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                selectedFile = openFileDialog1.FileName;
+                label1.Text = selectedFile.Split('\\').Last();
+            }
+        }
+
+        private void button_draw_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedFile)) return;
+
+            // Create 9x9 Cell Array
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var newEl = new Zelle();
+                    newEl.Location = new Point(i * 40, j * 40);
+                    this.Controls.Add(newEl);
+                }
+            }
         }
     }
 }
---
+++ 00_Str8tsGenerationAlgo/01_Code/Str8tsGenerationProject/Str8tsGenerationProject/Zelle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Str8tsGenerationProject
+{
+    public partial class Zelle : UserControl
+    {
+        public Zelle()
+        {
+            InitializeComponent();
+        }
+
+        [Category("Zelle"), Description("A property that controls the wossname")]
+        public int Zahl
+        {
+            get { return int.Parse(label1.Text); }
+            set { label1.Text = value.ToString(); }
+        }
+    }
+}
